373799f0f446d288922911b0673e8406488cbcec
gaoziqiang <gaoziqianghi@163.com>
2017-07-19 22:02:01 +0800

    DishUtil.java

---
+++ src/main/java/cn/edu/sdut/softlab/stream/DishUtil.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cn.edu.sdut.softlab.stream;
+
+import cn.edu.sdut.softlab.dish.Dish;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ *
+ * @author gaoziqiang
+ */
+public class DishUtil {
+
+    public static void main(String[] args) {
+        List<Dish> menu = Arrays.asList(
+                new Dish("pork", false, 800, Dish.Type.MEAT),
+                new Dish("beef", false, 700, Dish.Type.MEAT),
+                new Dish("pizza", false, 300, Dish.Type.OTHER)
+        );
+
+        List<Dish> lowCaloricDishes = new ArrayList<>();
+        for (Dish d : menu) {
+            if (d.getCalories() > 400) {
+                lowCaloricDishes.add(d);
+            }
+        }
+
+        Collections.sort(lowCaloricDishes, new Comparator<Dish>() {
+            @Override
+            public int compare(Dish d1, Dish d2) {
+                return Integer.compare(d1.getCalories(), d2.getCalories());
+            }
+        });
+
+        List<String> lowCaloricDishesName = new ArrayList<>();
+        for (Dish d : lowCaloricDishes) {
+            lowCaloricDishesName.add(d.getName());
+        }
+        for(String name : lowCaloricDishesName) {
+            System.out.println("热量大于400的食物:  " + name);
+        }
+    }
+}
